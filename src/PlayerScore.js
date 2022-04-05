dafb8d7981e815932a03a715db27501137188143
Harsheek Thanki <harsheekthanki@gmail.com>
2022-04-05 19:17:11 +0100

    CSS styling added

--- src/PlayerScore.js
+++ src/PlayerScore.js
@@ -2,19 +2,17 @@ import React from "react";
 
 function PlayerScore(props) {
   return (
-    <div>
-      <div className="player-scores">
-        {props.scores
-          .sort((a, b) => (a.s < b.s ? 1 : -1))
-          .map((score) => {
-            return (
-              <div className="player-score">
-                <div className="player">{score.n}</div>
-                <div className="score">{score.s}</div>
-              </div>
-            );
-          })}
-      </div>
+    <div className="player-scores">
+      {props.scores
+        .sort((a, b) => (a.s < b.s ? 1 : -1))
+        .map((score) => {
+          return (
+            <div className="player-score">
+              <div className="player">{score.n}</div>
+              <div className="score">{score.s}</div>
+            </div>
+          );
+        })}
     </div>
   );
 }
