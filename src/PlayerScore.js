aac2ae6e0c332702fa73044b0813735337b55fcc
Harsheek Thanki <harsheekthanki@gmail.com>
2022-04-05 18:20:31 +0100

    Level 3 complete

--- src/PlayerScore.js
+++ src/PlayerScore.js
@@ -4,14 +4,16 @@ function PlayerScore(props) {
   return (
     <div>
       <div className="player-scores">
-        {props.scores.map((score) => {
-          return (
-            <div className="player-score">
-              <div className="player">{score.n}</div>
-              <div className="score">{score.s}</div>
-            </div>
-          );
-        })}
+        {props.scores
+          .sort((a, b) => (a.s < b.s ? 1 : -1))
+          .map((score) => {
+            return (
+              <div className="player-score">
+                <div className="player">{score.n}</div>
+                <div className="score">{score.s}</div>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
